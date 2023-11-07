61bf383ba69a497929b8ea2ad187897afee7ce33
Lkhagvasuren Orgilsaikhan <23LP5770@G304-CC0025.local>
2023-11-07 20:30:37 +0800

    array easy medium duusgaw

---
+++ array.js
@@ -0,0 +1,116 @@
+// array easy
+
+// boglogo1
+
+// numbers = [10, 20, 30, 40];
+// let sum = 0;
+// let avarage;
+// for (i = 0; i < numbers.length; i++){
+//     sum += numbers[i];
+// }
+// avarage = sum / numbers.length
+// console.log(avarage)
+
+//bodlogo2 fibonacci
+
+// let fiboNumbers = [0,];
+// let n = 0;
+// let m = 1;
+// let sum = 0;
+
+// for(let i = 0; i < 100; i++){
+//     sum = n + m;
+//     n = m;
+//     m = sum
+//     fiboNumbers.push(n)
+//     console.log(i,':',n)
+// } console.log(fiboNumbers)
+
+
+
+//bodlogo3
+
+// numbers = [10, 20, 30, 40, 50];
+// numbers.pop();
+// numbers.push(60);
+// console.log(numbers)
+
+//bodlogo4
+
+// let numbers = [2, -3, 5, 6, -8, 10];
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++){
+//     if (numbers[i] > 0 && numbers[i] % 2 == 0){
+//         sum += numbers[i]
+//     }
+// } 
+// console.log(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//array medium
+
+
+
+
+
+// chunk
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// let arrays = 3;
+// const result = [];
+// for (let i = 0; i < numbers.length; i += arrays){
+//     result.push(numbers.slice(i, i + arrays))
+// } console.log(result)
+
+
+//find max and min
+
+// let numbers = [1, 3, 5, 22, 89, 46, 17, 31];
+// let min = numbers[0];
+// let max = numbers[0];
+// for(let i = 0; i < numbers.length; i++) {
+//     if(min > numbers[i]){
+//         min = numbers[i];
+//     } 
+//     if (max < numbers[i]){
+//         max = numbers[i]; 
+//     }
+// }
+// console.log(max, min);
+
+//sort an array
+
+// const numbers = [5, 100, 3, 2, 1];
+// for (let i = 0; i < numbers.length; i++){
+//     for (let j = 0; j < numbers.length; j++)
+//         if (numbers[i] < numbers[j]){
+//             var d = numbers[i];
+//             numbers[i] = numbers[j];
+//             numbers[j] = d;
+//         }
+    
+// } console.log(numbers)
+
+
+
+
+
+
+
+
+
+
+// array hard
+
+
