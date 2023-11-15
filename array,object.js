6a6f4d45f65f598c7e1777b2ef58429f74b043e6
Lkhagvasuren Orgilsaikhan <23LP5770@G304-CC0025.local>
2023-11-15 16:21:20 +0800

    11-15 checking gitub

--- array,object.js
+++ array,object.js
@@ -83,16 +83,124 @@
 
 // // 8
 
-let array = ['a', 1, 'b', 2];
-let numbersArrey = [];
-let stringsArray = [];
-for (let i = 0; i < array.length; i++){
-    if (array[i] = String){
-        stringsArray.push[array[i]]
+// let array = ['a', 1, 'b', 2];
+// let numbersArrey = [];
+// let stringsArray = [];
+// for (let i = 0; i < array.length; i++){
+//     if (array[i] = String){
+//         stringsArray.push[array[i]]
+//     }
+//     if (array[i] = Number){
+//         numbersArrey.push[array[i]]
+//         console.log(numbersArrey[i])
+//     }
+//     console.log(stringsArray, numbersArrey)
+// }
+
+
+// let array = [
+//     {firstName: 'Lkhagva', lastName: 'Orgil'},
+//     {firstName: 'Duk', lastName: 'Orgil'}
+// ];
+// let newArray = [];
+// for (let i = 0; i < array.length; i++){
+//     newArray[i] = array[i].firstName
+// }
+
+// console.log(array.map(el => el.firstName))
+
+// console.log(array.filter((el) => el.firstName === 'Lkhagva'))
+
+
+
+
+// map 1
+
+// let array = [2, 3, 4, 5];
+// console.log(array.map(function (el){
+//     return el * 2
+// }))
+// console.log(array.map(el => el * 2))
+
+// map 2
+
+// let celcius = [10];
+// console.log(array.map(function (el){
+//     return el * 1.8 + 32
+// }))
+// console.log(celcius.map(el => el * 1.8 + 32))
+
+// map 3
+// let array = [2, 3, 4, 5];
+// console.log(array.map(function (el) {
+//     return el ** 2
+// }))
+// console.log(array.map(el => el ** 2))
+
+// map 4
+
+// let array = ["lkhagva"];
+// console.log(array.map(function (el) {
+//     return el.toUpperCase()
+// }))
+// console.log(array.map(el => el.toUpperCase()))
+
+// map 5
+
+// let array = [{firstName: 'Lkhagva', lastName: 'Orgil'}];
+// console.log(array.map(function (el) {
+//     return el.firstName.charAt(0) + el.lastName.charAt(0)
+// }))
+// console.log(array.map(el => el.firstName.charAt(0) + el.lastName.charAt(0)))
+
+
+// filter 1
+
+// let array = [2, 3, 4, 5];
+// console.log(array.filter(function (el){
+//     return el % 2 === 0
+// }))
+// console.log(array.filter((el) => el % 2 === 0))
+
+// filter 2
+
+// let array = [-2, 3, -4, 5];
+// console.log(array.filter((el){
+//     return el > 0
+// }))
+// console.log(array.filter((el) => el > 0))
+
+// filter 3
+
+// let array = ['monster', 'ride', 'mom'];
+// let n = 3;
+// console.log(array.filter((el) => el.length > n))
+// console.log(array.filter(function(el){
+//     return el.length > n 
+// }))
+
+
+// filter 4
+
+
+const array = [1, 2, 3, 4, 5, 6, 8, 11, 22, 33];
+const IsItPrimeNumber = (x) => {
+    if (x === 1) return false
+    for (let i = 2; i < x; i++){
+        if (x % i === 0) return false
     }
-    if (array[i] = Number){
-        numbersArrey.push[array[i]]
-        console.log(numbersArrey[i])
-    }
-    console.log(stringsArray, numbersArrey)
-}
+    return true;
+};
+console.log(array.filter(function (el){
+    return IsItPrimeNumber(el)
+}))
+// console.log(array.filter(el => IsItPrimeNumber(el)))
+
+
+// filter 5
+
+// let array = ['monster', 'ride', 'mom'];
+// console.log(array.filter(function (el){
+//     return el[0] == "m"
+// }))
+// console.log(array.filter((el) => el[0] == "m"))
